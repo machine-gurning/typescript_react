319b493ccca85c92837ad4284ef4bdaeabae172e
Cody <c.e.yakimoff@gmail.com>
2022-09-17 10:10:41 +1000

    three quarters through

--- trello-clone/src/AddNewItem.tsx
+++ trello-clone/src/AddNewItem.tsx
@@ -1,22 +1,34 @@
-import React, {useState} from 'react';
+import React, {SyntheticEvent, useState} from 'react';
 import {AddItemButton} from "./styles";
+import {NewItemForm} from "./NewItemForm";
 
 type AddNewItemProps = {
     onAdd(text: string): void,
     toggleButtonText: string,
-    dark?: Boolean
+    dark?: boolean
 }
 
-    export const AddNewItem = ({onAdd, toggleButtonText, dark}: AddNewItemProps) => {
-    const [showForm, setShowForm] = useState(false);
+
+export const AddNewItem = ({onAdd, toggleButtonText, dark}: AddNewItemProps) => {
+    const [showForm, setShowForm] = useState(false)
 
     if (showForm) {
-
+        // Render the form
+        return (
+            <NewItemForm onAdd={(text) => {onAdd(text); setShowForm(false)}}/>
+        )
     }
 
+    const handleOnKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Escape') {
+            setShowForm(false)
+        }
+    }
+
+    // Else we will render the "add new" button
     return (
         <AddItemButton dark={dark} onClick={()=> setShowForm(true)}>
             {toggleButtonText}
         </AddItemButton>
-        )
+    )
 }
--- trello-clone/src/App.tsx
+++ trello-clone/src/App.tsx
@@ -1,8 +1,19 @@
 import {AppContainer, ColumnContainer, CardContainer, ColumnTitle} from './styles'
 import {Column} from "./Column";
 import {Card} from "./Card";
+import {AddNewItem} from "./AddNewItem";
+
+// Make a reducer function for which the payload is:
+// {type: ADD_CARD or ADD_COLUMN, value: text column: column_number/name?}
+//
+
 
 export const App = () => {
+    const  = () => {
+
+    }
+
+
   return (
       <AppContainer>
           <Column text= "Test title">
@@ -10,12 +21,15 @@ export const App = () => {
               <Card text="This is the writing on the card"/>
               <Card text="This is the writing on the card"/>
               <Card text="This is the writing on the card"/>
+              <AddNewItem onAdd={()=> {console.log("item created")}} toggleButtonText="+ Add new item" dark={true}/>
           </Column>
           <Column text="old mate">
               <Card text="This is the writing on the card"/>
               <Card text="This is the writing on the card"/>
               <Card text="This is the writing on the card"/>
+              <AddNewItem onAdd={()=> {console.log("item created")}} toggleButtonText="+ Add new item" dark={true}/>
           </Column>
+          <AddNewItem onAdd={(e) => console.log(e)} toggleButtonText="+ Add new column" dark={false}/>
       </AppContainer>
   );
 }
